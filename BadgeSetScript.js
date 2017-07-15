c64201e408a57ca3b313d2aae454f5726a200828
Shadowfyst997 <dragonlord997@hotmail.com>
2017-07-15 05:33:56 -0600

    Update BadgeSetScript.js

--- BadgeSetScript.js
+++ BadgeSetScript.js
@@ -612,7 +612,8 @@ if(window.location.href.indexOf('topic') == 32 || window.location.href.indexOf('
 		addBadge("Nedben", "CharacterFundamentStellar", "2016/12/10");
 		addBadge("Nedben", "CharacterBuildCaster", "2016/12/10");
 		addBadge("Nedben", "CharacterClassNedben", "2016/12/10");
-		addBadge("Nedben", "ActivityWriter10", "2017/01/03"); //11 submissions
+		addBadge("Nedben", "ActivityWriter10", "2017/01/03"); //11 Submissions
+		addBadge("Nedben", "ActivityCodexCreator05", "2017/07/15"); //5 Submissions
 		addBadge("Nedben", "AllianceCompWinner01", "2017/05/28");
 		addBadge("Nedben", "EventValentine2017", "2017/02/14");
 		addBadge("Nedben", "EventHalloween2016", "2016/10/31");
@@ -626,7 +627,7 @@ if(window.location.href.indexOf('topic') == 32 || window.location.href.indexOf('
 		addBadge("Neshi", "CharacterClassNeshi", "2016/12/10");
 		addBadge("Neshi", "VenusDjinn", "2017/06/14");
 		addBadge("Neshi", "ActivityTopicMaker040", "2015/08/21");
-		addBadge("Neshi", "ActivityArtist25", "2017/06/14"); //25 Submissions
+		addBadge("Neshi", "ActivityArtist25", "2017/06/14"); //26 Submissions
 		addBadge("Neshi", "ActivityMusician01", "2017/06/14"); //1 Submissions
 		addBadge("Neshi", "GameBadArt", "2017/05/28");
 		//addBadge("Neshi", "ClubBadArtist", "2016/06/11");
